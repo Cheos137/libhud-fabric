a38b5375066999a18fbcb9ae82549f04a50d2dd3
Cheos <cheoscrafter@gmail.com>
2022-12-08 19:14:52 +0100

    more mod setup

---
+++ src/main/java/dev/cheos/libhud/Libhud.java
@@ -0,0 +1,12 @@
+package dev.cheos.libhud;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Libhud {
+	public static final Logger LOGGER = LogManager.getLogger("libhud");
+	
+	public void client() {
+		
+	}
+}
